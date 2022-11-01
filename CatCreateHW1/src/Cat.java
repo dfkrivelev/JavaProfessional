d27699b8864747b7f9c68512467d919d746cdb5c
Dzmitry Kryvialeu <111528094+dfkrivelev@users.noreply.github.com>
2022-11-01 13:54:56 +0100

    create a cat

---
+++ CatCreateHW1/src/Cat.java
@@ -0,0 +1,24 @@
+public class Cat {
+    String name;
+    String color;
+    int age;
+
+    public Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public void voice(){
+        System.out.println("meow");
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ CatCreateHW1/src/CatCreateHW1.java
@@ -0,0 +1,7 @@
+public class CatCreateHW1 {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Morty", "Black", 10);
+        cat.voice();
+        System.out.println(cat);
+    }
+}
