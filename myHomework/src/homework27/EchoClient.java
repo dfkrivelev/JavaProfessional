439210726f3462e326858cbd43db9fda98716984
Dzmitry Kryvialeu <111528094+dfkrivelev@users.noreply.github.com>
2023-02-18 15:12:49 +0100

    homework27

---
+++ myHomework/src/homework27/EchoClient.java
@@ -0,0 +1,31 @@
+package homework27;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoClient {
+    public static void main(String[] args) {
+        new EchoClient();
+    }
+
+    public EchoClient() {
+        String message = null;
+        try (Socket socket = new Socket("localhost", 2048);
+             PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            System.out.println("Connection to server...");
+            Scanner scanner = new Scanner(System.in);
+            do {
+                System.out.println("# ");
+                message = scanner.nextLine();
+                writer.println(message);
+                writer.flush();
+                System.out.println(reader.readLine());
+            } while (!message.equals("exit"));
+            System.out.println("Connection closed.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ myHomework/src/homework27/EchoServer.java
@@ -0,0 +1,45 @@
+package homework27;
+
+/**
+ * Java Prof. Homework #27
+ *
+ * @author Dzmitry Kryvialeu
+ * @version 16.02 - 18.02
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+    public static void main(String[] args) {
+        new EchoServer();
+    }
+
+    public EchoServer() {
+        try (ServerSocket server = new ServerSocket(2048)) {
+            System.out.println("Server started...");
+            while (true) {
+                Socket socket = server.accept();
+                System.out.println("client connected...");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter writer = new PrintWriter(socket.getOutputStream());
+                String message = null;
+                do {
+                    message = reader.readLine();
+                    System.out.println("Client say: " + message);
+                    writer.println("Echo " + message);
+                    writer.flush();
+                } while (!message.equals("exit"));
+                reader.close();
+                writer.close();
+                System.out.println("client disconnect.");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
